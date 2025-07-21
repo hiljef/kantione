253718dd9289bdfff57b2e36d7341d4bd9eac2b4
Hilary Jeffery <hil@hiljef.com>
2025-07-22 01:03:24 +0200

    kantione_site

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,36 @@ import Layout from '../components/layout'
 const IndexPage = () => {
   return (
     <Layout pageTitle="kantione">
-        <p> a new concert series curated and organised by <b>Eleni Poulou</b> </p>
-         <p> <b>Kantione</b> was launched in 2024 to present new forms of classical music at <b>Haus am Waldsee</b> in Berlin-Zehlendorf</p>
-
+        a new concert series curated and organised by <b>Eleni Poulou</b>
+        <br /><br /> 
+        <b>kantione</b> was launched in 2024 to present new forms of classical music at <b>Haus am Waldsee</b> in Berlin-Zehlendorf
+        <br /><br />
+        each concert features new combinations of artists, premiering their work in an intimate setting
+        <br /><br />
+        most of the musicians have already made music together from classical to jazz, improvised to folk, as well being experienced performers of experimental music in an art context with artists such as CC Hennix, Rebecca Horn, Anselm Kiefer and Anri Sala
+        <br /><br />
+        they can usually heard on big stages such as the Berlin Philharmonic, Konzerthaus, at jazz clubs and international festivals, seldom can their art be enjoyed close up in this way
+         <br /><br />
+         as proven in the first series in 2024, this form of concert appeals to a wide and diverse public, presented in a contemporary art context
+         <br /><br />
+         thanks to the generous support of the friends and patrons of Haus am Waldsee, the series continues in 2025, beginning on 21 August
+         <br /><br />
+         <a href="https://hausamwaldsee.de/freunde-und-foerderer/" target="_blank" rel="noopener noreferrer">hausamwaldsee.de/freunde-und-foerderer/</a>
+         <br /><br />
+         <b>Haus am Waldsee</b><br />
+	 Argentinische Allee 30<br />
+         14163 Berlin-Zehlendorf
+         <br /><br />
+         Bus<br />
+	 118  Rathaus Zehlendorf<br />
+	 622 / X11 Krumme Lanke
+	 <br /><br />
+	 Bahn<br />
+	 U3 Krumme Lanke<br />
+	 S1 Mexikoplatz
+	 <br /><br />
+	 <a href="https://hausamwaldsee.de/" target="_blank" rel="noopener noreferrer">hausamwaldsee.de</a>
+	<br /><br /><br /><br />	
         </Layout>
   )
 }
--- src/pages/konzerten.js
+++ src/pages/konzerten.js
@@ -7,7 +7,17 @@ const KonzertenPage = () => {
   return (
     <main>
       <Layout pageTitle="konzerten">
-      <p>2025</p>
+      	2025<br />
+      	<p><b>Thursday 21 August</b><br />
+       	7pm<br /><br />
+       	<b>Wabi Sabi Barock</b>
+       	<br /><br />
+	Hilary Jeffery – trombone, trumpet<br />
+	Martin Heinze – double bass
+	<br /><br />
+	The duo Martin Heinze and Hilary Jeffery interpret early music in a distinctive, open style, drawing on the 					Japanese concept of <i>Wabi Sabi</i> - a notion that values imperfection, asymmetry, roughness, irregularity, and simplicity. In this concert, they present new versions of Orlando di Lasso’s <i>Cantione Sine Textu</i> together with a revised version of their signature piece <i>Wabi Sabi à la Ortiz</i>.
+	<br /><br />
+       </p>
       </Layout>
     </main>
   )
